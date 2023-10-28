3d348f565844ca14e430c93c34fa25ab3124fd76
Vivek <vivek.mitra@outlook.com>
2023-10-28 09:09:28 +0530

    code doco/plan created

---
+++ logic.js
@@ -0,0 +1,10 @@
+//The logic of this code is to emulate a game of tic-tac-toe. It will be a game
+//of three rounds. Best of three rounds wins
+//Code plan - a function to receive and return the choice of the player.
+        //  - a function to receive and return the choice of ai mode
+        //  - a game controller function to switch between player choice and ai choice and return game state
+        //  - a function to keep track of score based on game state and display results
+        //  - a function to implement the type of AI based on player choice (minimax algorithm)
+//Now each line may have more functions
+
+
