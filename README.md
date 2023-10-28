7792738fc5d4474f8e39c2a425d41dc939a2288a
Vivek <vivek.mitra@outlook.com>
2023-10-28 09:16:45 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tic-Tac-Toe
 Simple game to play tic-tac-toe either against your friend or an AI overlord 
 - This is a single player game against AI
-- You can choose to play in three levels against an AI - amateur, professional or veteran
+- You can choose to play in three levels against an AI - easy, medium or overlord
 - Win 3 / 5 rounds to win the game
 ![Figma Skeleton for Web UI](./Tic-Tac-Toe.png)
--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 //The logic of this code is to emulate a game of tic-tac-toe. It will be a game
-//of three rounds. Best of three rounds wins
+//of three rounds. Best of three out of 5 rounds wins
 //Code plan - a function to receive and return the choice of the player.
         //  - a function to receive and return the choice of ai mode
         //  - a game controller function to switch between player choice and ai choice and return game state
