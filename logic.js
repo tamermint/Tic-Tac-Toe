d83fbe17aee0d06c3849794b9bca25ddb5641e0a
Vivek <vivek.mitra@outlook.com>
2023-11-04 19:53:26 +0530

    minimax logic added, however, WIP

--- logic.js
+++ logic.js
@@ -34,26 +34,27 @@ function determineAiPlayerMode(aiPlayer) {       //check what the human is playi
     return aiPlayer;
 }
 
-function minimax(currentBoard, player, difficultyScore) {
-    //function to determine the best move AI should make, check for terminal states
+function minimax(currentBoard, player) {         //function to determine the best move AI should make, check for terminal states
+
+    //loop through all available cells in board
     let availCells = currentBoard.filter(cell => cell != 'X' && cell != 'O');
-    if (difficultyScore == 2) {
-        if(winConditions(currentBoard, player) && player == 'aiPlayer'){
-            return {score: 10};
-        }
-        else if(winConditions(currentBoard, player) && player == 'humanPlayer') {
-            return {score: -10};
-        }
-        else if(availCells.length === 0) {
-            return {score: 0};
-        }
+    
+    if(winConditions(currentBoard, player) && player == 'aiPlayer'){
+        return {score: 10};
     }
+    else if(winConditions(currentBoard, player) && player == 'humanPlayer') {
+        return {score: -10};
+        }
+    else if(availCells.length === 0) {
+        return {score: 0};
+        }
+
     const moves = [];       //empty array to store each state of the board
 
     //for each space in the availCells array
     for(let i = 0; i < availCells.length; i++){
         //create a move{} object for each move
-        var move = {};
+        let move = {};
 
         //store index of the current empty spot in move{}
         move.index = currentBoard[availCells[i]];
@@ -63,13 +64,13 @@ function minimax(currentBoard, player, difficultyScore) {
 
         //if player == human player, store result of minimax(currentBoard, player, difficultyScore) of the opponent in a move.score
         if(player == 'humanPlayer') {
-            var result = minimax(currentBoard, 'aiPlayer',);
+            let result = minimax(currentBoard, 'aiPlayer',);
             move.score = result.score;
         } 
 
         //else store result of minimax(currentBoard, player, difficultyScore) in move.score
         else {
-            var result = minimax(currentBoard, 'humanPlayer',);
+            let result = minimax(currentBoard, 'humanPlayer',);
             move.score = result.score;
         }
 
