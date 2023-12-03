5d197ef86169f5f88bf1e2eef4c882705cff852a
Vivek <vivek.mitra@outlook.com>
2023-12-03 15:35:55 +1100

    logic changes added to JS and code refactored a bit

--- logic.js
+++ logic.js
@@ -8,129 +8,102 @@
 //Now each line may have more functions
 
 const isWon = false;         //to check if win condition was acheived by either human/AI
-const gameState = false;     //to track state of game
+const gameState = true;     //to track state of game
 let humanScore = 0;          //track score of human
 let aiScore = 0;             //track score of AI
+let gameRounds = 0;          //to keep a track of the game round
+let winScore = 5;            //to track whether human has won or AI has won
 
-function playerChoice() { //function to take a player's choice and append player choice (i.e. X or O) to any game cell
-    const gameContainer = document.getElementById('game-container');
+function playerChoice() { //function to determine player's choice i.e. play as X or play as O
+    // const gameContainer = document.getElementById('game-container');
+    const playChoice = document.getElementById('player').value;
 
-    gameContainer.addEventListener('click', function(event){     //attach event listener to the parent and then delegate based on area clicked
+    /* gameContainer.addEventListener('click', function(event){     //attach event listener to the parent and then delegate based on area clicked
         if(event.target.classList.contains('cell')) {            //check if where the user has clicked is indeed a cell
             const cell = event.target;
 
             //check if cell is empty before placing a player's choice
             if(cell.textContent === ''){
-                const playChoice = document.getElementById('player').value;
+                
                 cell.textContent = playChoice;
             }
         }
-    })
+    }) */
+    return playChoice;
 }
 
-function determineAiPlayerMode(aiPlayer) {       //check what the human is playing as
-    const playChoice = document.getElementById('player').value;
-    playChoice == 'X' ? aiPlayer = 'O' : 'X';
-    return aiPlayer;
+function determineAiPlayerMode() {       //check what the human is playing as choose the opposite 
+    const playChoice = playerChoice();
+    return aiPlayer = (playChoice == 'X' ? 'O' : 'X');   
 }
 
-function minimax(currentBoard, player) {         //function to determine the best move AI should make, check for terminal states
-
-    //loop through all available cells in board
+function minimaxWithDepth(currentBoard, player, depth) {
     let availCells = currentBoard.filter(cell => cell != 'X' && cell != 'O');
-    
-    if(winConditions(currentBoard, 'aiPlayer')){
+
+    const aiPlayer = determineAiPlayerMode();
+    const humanPlayer = playerChoice();
+
+    if(winConditions(currentBoard, aiPlayer)) {
         return {score: 10};
     }
-    else if(winConditions(currentBoard, 'humanPlayer')) {
+    else if(winConditions(currentBoard, humanPlayer)) {
         return {score: -10};
-        }
-    else if(availCells.length === 0) {
+    }
+    else if(availCells.length === 0){
         return {score: 0};
-        }
+    }
 
-    const moves = [];       //empty array to store each state of the board
+    const moves = [];              //create an empty array to store moves
 
-    //for each space in the availCells array
-    for(let i = 0; i < availCells.length; i++){
-        //create a move{} object for each move
-        let move = {};
+    for(let i = 0; i < availCells.length; i++) {
+        
+        var move = {};             //create a move object to store the move of each empty spot
 
-        //store index of the current empty spot in move{}
-        move.index = currentBoard[availCells[i]];
+        move.index = currentBoard[availCells[i]];       //set index of the move to current empty spot
 
-        //set empty spot to the current player
-        currentBoard[availCells[i]] = player;
+        currentBoard[availCells[i]] = player;           //set the empty spot to the player
 
-        //if player == human player, store result of minimax(currentBoard, player) of the opponent in a move.score
-        if(player == 'humanPlayer') {
-            let result = minimax(currentBoard, 'aiPlayer');
+        if(player == humanPlayer) {
+            var result = minimaxWithDepth(currentBoard, aiPlayer, depth - 1)    //get the result of calling the minimax with depth if the opponent is human
             move.score = result.score;
-        } 
-
-        //else store result of minimax(currentBoard, player, difficultyScore) in move.score
+        }
         else {
-            let result = minimax(currentBoard, 'humanPlayer');
+            var result = minimaxWithDepth(currentBoard, humanPlayer, depth - 1)
             move.score = result.score;
         }
 
-        //reset the spot to empty
+        //reset the board
         currentBoard[availCells[i]] = move.index;
 
-        //push move into moves[]
+        //push the move into the moves array
         moves.push(move);
 
-        //use a bestMove variable to evaluate the best move
-        let bestMove;
-
-        if(player === 'aiPlayer')
-            bestMove = -10000
-            for(let i = 0; i < moves.length; i++) {
-                if (moves[i].score > bestMove) 
-                    bestMove = moves[i].score
-            }
-        if (player === 'humanPlayer')
-            bestMove = 10000
-            for(let i = 0; i < moves.length; i++) {
-                if (moves[i].score < bestMove)
-                    bestMove = moves[i].score
-            }
     }
-    return moves[bestMove];
-}
 
-function aiPlayerChoice(currentBoard) { // takes the current board after the player has made the choice, and based on AI difficulty level, run minimax on available spots and fill in the choice
-    const aiPlayerMode = determineAiPlayerMode(aiPlayer); 
-    const aiDifficultyLevel = document.getElementById('ai').value;
-    const gameContainer = document.getElementById('game-container');
-
-    if(aiDifficultyLevel == 'easy') {
-        const randomInput = () => Math.floor(Math.random() * (8 - 0 + 1)) + 0;
-        const randomIndex = randomInput();
-        let availCells = currentBoard.filter(cell => cell != 'X' && cell != 'O');
-        for(let i = 0; i < availCells.length; i++) {
-            if(availCells[randomIndex] == '') {
-                availCells[randomIndex] == aiPlayerMode;
-            }
-            else if(availCells.length == 0 && winConditions(currentBoard, 'humanPlayer')) {
-                humanScore += 1;
-                window.alert("You win this round!");
-            }
-            else if(availCells.length == 0 && winConditions(currentBoard, 'aiPlayer')) {
-                aiScore += 1;
-                window.alert("AI wins this round!");
+    let bestMove;               //variable to select the best move
+    if(player == aiPlayer) {       //if the player is aiPlayer, use a variable to get the maximizing move for AI
+        let bestScore = -Infinity;
+        for(let i = 0; i < moves.length; i++) {
+            if(moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
             }
         }
-
-    }
-    else if(aiDifficultyLevel == 'medium') {
-        //use a depth-limited minimax
     }
     else {
-        //the player is unable to win or results in tie
+        let bestScore = Infinity;   //if player is humanPlayer, use a variable to get the minimizing move for AI
+        for(let i = 0; i < moves.length; i++) {
+            if(moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
     }
-    return gameContainer;
+    return moves[bestMove];
+
 }
+
+
 
 function winConditions(currentBoard, player) {
     if (
@@ -146,6 +119,43 @@ function winConditions(currentBoard, player) {
             return true
     }
     return false;
+}
+
+function checkTie(currentBoard, player) {
+    if(!winConditions(currentBoard, player)) {
+        currentBoard.every((cell => {
+            if(cell.value != '') {
+                cell.style.backgroundColor = 'blue';
+                window.alert("It's a tie!")
+            }
+        }))
+    }
+}
+
+function checkForWin() {
+    //check if player (ai or human) has won 3/5 games, if yes, declare them tournament winner and update game state to stop game
+    if(winScore - aiScore == 2) {
+        window.alert("AI wins the tournament!");
+        gameState = false;
+    }
+    else if(winScore - humanScore == 2) {
+        window.alert("Human wins the tournament!");
+        gameState = false;
+    }
+}
+
+function gameController() {        //function to control game flow
+    while(gameRounds <= 5) {       //game has 5 rounds and best of 3 rounds wins!
+       //check whether game state is false
+        if(gameState) {
+            playerChoice();
+            aiPlayerChoice(currentBoard);
+        }
+        else {
+            checkForWin();
+        }
+        gameRounds++;              //increment till 5 
+    }
 }
 
 
