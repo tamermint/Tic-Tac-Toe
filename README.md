213180c0f5e11a064203bf6eb6e42847319a7559
Vivek <vivek.mitra@outlook.com>
2023-12-09 15:39:25 +1100

    logic to add ai choice added, refactored code for checking tournament winner and round winner

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Simple game to play tic-tac-toe either against your friend or an AI overlord
 - You can choose to play in three levels against an AI - easy, medium or overlord
 - Win 3 / 5 rounds to win the game
 ![Figma Skeleton for Web UI](./Tic-Tac-Toe.png)
+
+- Doco :
+ - newGame() - resets the state to initial conditions
+ - restartGame() - maintains the state but clears the board
+ - setupBoard() - uses restartGame() and calls playerChoice();
+ - playerChoice() - listens for the mouse click and adds in either X or O
+ - endRound() - increments the game rounds and checks whether the rounds have reached 5. If 
+    gameRounds != 5, it calls setupBoard()
+ - winConditions() - checks for the winning conditions
+ - checkTie() - checks whether its a tie
+ - aiChoice() - utilizes depth limited minimax algorithm and plays agains you
+ - checkForRoundWinner() and checkTournamentWinner() - checks for round and tournament winner
+ - gameController() - calls the setupBoard() and playerChoice();
--- logic.js
+++ logic.js
@@ -1,13 +1,8 @@
 //The logic of this code is to emulate a game of tic-tac-toe against an AI. It will be a game
-//of three rounds. Best of three out of 5 rounds wins
-//Code plan - a function to receive and return the choice of the player.
-        //  - a function to receive and return the choice of ai mode
-        //  - a game controller function to switch between player choice and ai choice and return game state
-        //  - a function to keep track of score based on game state and display results
-        //  - a function to implement the type of AI based on player choice (minimax algorithm)
-//Now each line may have more functions
+//of five rounds. Highest score wins
 
-const isWon = false;         //to check if win condition was acheived by either human/AI
+
+
 const gameState = true;     //to track state of game
 let humanScore = 0;          //track score of human
 let aiScore = 0;             //track score of AI
@@ -19,12 +14,52 @@ const humanPlayer = document.getElementById('player').value;      //get value fr
 const aiPlayer = (humanPlayer == 'X' ? 'O' : 'X');                //set aiPlayer
 const aiLevel = document.getElementById('ai').value; 
 
-function playerChoice() {
-    document.querySelector('#game-container').addEventListener('click', function(){
-        document.querySelectorAll('.cell').forEach(cell => cell.innerText = humanPlayer);
-    });
+function newGame() {         //resets all counters and conditions to initial value and clears the board                               
+    gameState = true;       //to track state of game
+    humanScore = 0;          //track score of human
+    aiScore = 0;             //track score of AI
+    gameRounds = 0;          //to keep a track of the game round
+    winScore = 5;            //to track whether human has won or AI has won
+    restartGame();
 }
 
+function restartGame() {           //resets the current game
+    const gameBoard = document.querySelectorAll('.cell');
+    gameBoard.forEach(cell => cell.innerText = '');
+}
+
+
+function setupBoard() {
+    restartGame();
+    playerChoice();
+}
+
+function endRound() {               //end the round
+    gameRounds++;                   //increment gameRounds
+    updateScoreDisplay();          //update the display
+    if(gameRounds < 5) {
+        setTimeout(setupBoard, 2000);          
+    }
+    else {
+        checkForTournamentWinner();
+    }
+}
+
+function winConditions(currentBoard, player) {       //set the winning conditions for the board i.e. the cells 
+    if (
+        (currentBoard[0] == player && currentBoard[1] == player && currentBoard[2] == player) ||
+        (currentBoard[3] == player && currentBoard[4] == player && currentBoard[5] == player) ||
+        (currentBoard[6] == player && currentBoard[7] == player && currentBoard[8] == player) ||
+        (currentBoard[0] == player && currentBoard[3] == player && currentBoard[6] == player) ||
+        (currentBoard[1] == player && currentBoard[4] == player && currentBoard[7] == player) ||
+        (currentBoard[2] == player && currentBoard[5] == player && currentBoard[8] == player) ||
+        (currentBoard[0] == player && currentBoard[4] == player && currentBoard[8] == player) ||
+        (currentBoard[2] == player && currentBoard[4] == player && currentBoard[6] == player)
+        ) {
+            return true
+    }
+    return false;
+}
 
 function minimaxWithDepth(currentBoard, player, depth) {          //set up the minimax function with depth to control difficulty
     let availCells = currentBoard.filter(cell => cell != 'X' && cell != 'O');
@@ -89,80 +124,96 @@ function minimaxWithDepth(currentBoard, player, depth) {          //set up the m
 
 }
 
-
-
-function winConditions(currentBoard, player) {       //set the winning conditions for the board i.e. the cells 
-    if (
-        (currentBoard[0] == player && currentBoard[1] == player && currentBoard[2] == player) ||
-        (currentBoard[3] == player && currentBoard[4] == player && currentBoard[5] == player) ||
-        (currentBoard[6] == player && currentBoard[7] == player && currentBoard[8] == player) ||
-        (currentBoard[0] == player && currentBoard[3] == player && currentBoard[6] == player) ||
-        (currentBoard[1] == player && currentBoard[4] == player && currentBoard[7] == player) ||
-        (currentBoard[2] == player && currentBoard[5] == player && currentBoard[8] == player) ||
-        (currentBoard[0] == player && currentBoard[4] == player && currentBoard[8] == player) ||
-        (currentBoard[2] == player && currentBoard[4] == player && currentBoard[6] == player)
-        ) {
-            return true
-    }
-    return false;
-}
-
 function checkTie(currentBoard, player) {           //check for whether the game is a tie
-    if(winConditions(currentBoard, player) == false) {
-        if(currentBoard.every((cell => cell.value != ''))) {
+    if(!winConditions(currentBoard, player)) {
+        if(currentBoard.every((cell => cell.innerText != ''))) {
             window.alert("It's a tie!")
-            gameState = false;
+            restartGame();
         }   
     }
 }
 
-function restartGame() {           //resets the current game
-    const gameBoard = document.querySelectorAll('.cell');
-    gameBoard.forEach(cell => cell.innerText = '');
+
+function playerChoice() {
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.addEventListener('click', function() {
+            if(cell.innerText === '') {
+                cell.innerText = humanPlayer;
+                // After player move, call the AI move and update the game state
+                if(checkForRoundWinner() || checkTie()){
+                    endRound();
+                }
+                else{
+                    aiChoice();
+                }
+            }
+        });
+    });
 }
 
-function newGame() {         //resets all counters and conditions to initial value and clears the board  
-    isWon = false;          //to check if win condition was acheived by either human/AI
-    gameState = true;       //to track state of game
-    humanScore = 0;          //track score of human
-    aiScore = 0;             //track score of AI
-    gameRounds = 0;          //to keep a track of the game round
-    winScore = 5;            //to track whether human has won or AI has won
-    restartGame();
+function aiChoice() {
+    const cells = document.querySelectorAll('.cell');  //get all the cells
+    const currentBoard = Array.from(cells, cell => cell.innerText); //current board is a game array that has all the X's and O's
+ 
+    let move;       //ai's move
+     if(aiLevel === 'easy') {
+         move = minimaxWithDepth(currentBoard, aiPlayer, 1);
+     }
+     else if(aiLevel === 'medium') {
+         move = minimaxWithDepth(currentBoard, aiPlayer, 3);
+     }
+     else {
+         move = minimaxWithDepth(currentBoard, aiPlayer, 10);
+     }
+ 
+     //let's apply AI's move
+     if(move && move.index !== undefined) {
+         cells[move.index].innerText = aiPlayer;
+     }
 }
 
+function updateScoreDisplay() {
+    const aiScoreDisplay = document.querySelector('#ai-score');
+    const humanScoreDisplay = document.querySelector('#player-score');
+    
+    aiScoreDisplay.innerText = aiScore;
+    humanScoreDisplay.innerText = humanScore;
+}
+
+function checkForTournamentWinner() {
+    if (humanScore > aiScore) {
+        window.alert("Human wins the tournament!");
+    }
+    else if(aiScore > humanScore) {
+        window.alert("AI wins the tournament!");
+    }
+    else {
+        window.alert("The tournament ends in a tie!")
+    }
+}
 
 function checkForRoundWinner() {                     //check for winner for current round and update display on UI
     if(winConditions(currentBoard, aiPlayer)) {
         aiScore += 1;
         window.alert("AI wins this round!");
-        const aiScoreDisplay = document.querySelector('#ai-score');
-        aiScoreDisplay.innerText = aiScore;
         restartGame();
     }
     if(winConditions(currentBoard, humanPlayer)) {
         humanScore += 1;
         window.alert("Human wins this round!");
-        const humanScoreDisplay = document.querySelector('#player-score');
-        humanScoreDisplay.innerText = humanScore;
         restartGame();
     }
 }
 
+
 function gameController() {        //function to control game flow
-    while(gameRounds <= 5) {       //game has 5 rounds and best of 3 rounds wins!
-       //check whether game state is false
-        if(gameState) {
-            playerChoice();
-            aiPlayerChoice(currentBoard);
-        }
-        else {
-            checkForWin();
-        }
-        gameRounds++;              //increment till 5 
-    }
+    setupBoard();
+    playerChoice();
 }
 
-document.querySelector('#new-game').onClick = newGame();
-document.querySelector('#restart-game').onClick = restartGame();
+document.addEventListener('DOMContentLoaded', function() {
+    gameController();
+});
 
+document.querySelector('#new-game').onclick = newGame;
+document.querySelector('#restart-game').onclick = restartGame;
